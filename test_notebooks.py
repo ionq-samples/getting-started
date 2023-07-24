944b9b36f0e3a16e665fd6e494dea4ccea12a6d7
Spencer Churchill <spence@duck.com>
2023-07-23 17:04:44 -0700

    Revert "fix workflow (#25)"
    This reverts commit 97667b8a26cf21f82017266120f5ee04a050c383.

--- test_notebooks.py
+++ test_notebooks.py
@@ -1,33 +1,46 @@
 import os
+import subprocess
 import nbformat
 import glob
-import subprocess
 
 
 def test_notebook(path):
-    # Run the notebook
-    res = subprocess.run(
-        ["python3", "-m", "nbconvert", "--to", "html", "--execute", path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
+    # Reading the notebook
+    nb = nbformat.read(path, as_version=4)
 
-    # If the notebook ran without errors, the return code will be 0
-    if res.returncode != 0:
-        raise Exception(
-            f"Error executing the notebook {path}. Output:\n{res.stdout.decode('utf-8')} {res.stderr.decode('utf-8')}"
-        )
+    proc = subprocess.run(
+        [
+            "jupyter",
+            "nbconvert",
+            "--to",
+            "notebook",
+            "--execute",
+            "--ExecutePreprocessor.kernel_name=python3",
+            "--ExecutePreprocessor.timeout=60",
+            "--output",
+            "/dev/null",
+            path,
+        ],
+        capture_output=True,
+    )
+    # return the result of the subprocess instead of asserting
+    return proc
 
 
 def test_notebooks():
-    # Get a list of all notebook files
-    notebooks = glob.glob("**/*.ipynb", recursive=True)
+    for notebook in glob.glob("**/*.ipynb", recursive=True):
+        yield notebook
 
-    # Test each notebook
-    for notebook in notebooks:
-        print(f"Testing {notebook}")
-        test_notebook(notebook)
+
+def main():
+    for notebook in test_notebooks():
+        result = test_notebook(notebook)
+        if result.returncode != 0:
+            print(f"The notebook {notebook} failed with the following error:\n")
+            # print the stdout and stderr if the notebook failed to execute
+            print(result.stdout.decode())
+            print(result.stderr.decode())
 
 
 if __name__ == "__main__":
-    test_notebooks()
+    main()
